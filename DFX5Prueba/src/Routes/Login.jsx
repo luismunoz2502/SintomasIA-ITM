e852be8b630afbc5815dbb772d8c9fa48929a084
Luis Munoz <Ldavid.munoz@udea.edu.co>
2024-07-04 12:50:30 -0500

    fist commit

--- DFX5Prueba/src/Routes/Login.jsx
+++ DFX5Prueba/src/Routes/Login.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-
+import { useState } from "react";
 
 export default function Login() {
+  const [Username,setUsername]= useState ("");
+  const [Password,setPassword]= useState ("");
+
+  
   return (
+
+
+    
     <div className="login-container">
       <form className="login-form">
         <h1>Login</h1>
         <div className="form-group">
           <label>Username</label>
-          <input type="text" />
+          <input type="text" value={Username} onChange={(e)=>setUsername(e.target.value)}/>
         </div>
         <div className="form-group">
           <label>Password</label>
-          <input type="password" />
+          <input type="password" value={Password} onChange={(e)=>setPassword(e.target.value)}/>
         </div>
         <button className="login-button">Login</button>
       </form>
--- DFX5Prueba/src/Routes/signup.jsx
+++ DFX5Prueba/src/Routes/signup.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 export default function SignUp () 
-{ return(
+{ const [Username,setUsername]= useState ("");
+  const [Email,setEmail]= useState ("");
+  const [Password,setPassword]= useState ("");
+
+  
+  
+  return(
     <div className="login-container">
       <form className="login-form">
         <h1>SingUp</h1>
         <div className="form-group">
-          <label></label>
-          <input type="text" />
+          <label>Username</label>
+          <input type="text" value={Username} onChange={(e)=>setUsername(e.target.value)}/>
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input type="text" value={Email} onChange={(e)=>setEmail(e.target.value)}/>
         </div>
         <div className="form-group">
           <label>Password</label>
-          <input type="password" />
+          <input type="password" value={Password} onChange={(e)=>setPassword(e.target.value)}/>
         </div>
-        <button className="login-button">SingUp</button>
+        
+        <button className="login-button">Create user</button>
       </form>
     </div>
   );;
